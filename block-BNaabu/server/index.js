92e66c79ffa04edb3b97abce763c3fef7aa1c8f9
Ritika Agrawal <ritikaagrawal339@gmail.com>
2023-05-10 20:45:48 +0530

    complete: Error Handler Middlewares

---
+++ block-BNaabu/server/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Home!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Welcome to About Page!");
+});
+
+app.use((req, res, next) => {
+  console.log(req.url);
+  if (req.url === "/admin") {
+    return next("Unauthorized");
+  }
+
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send("Page not found");
+});
+
+app.use((err, req, res, next) => {
+  res.send(err);
+  next();
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
