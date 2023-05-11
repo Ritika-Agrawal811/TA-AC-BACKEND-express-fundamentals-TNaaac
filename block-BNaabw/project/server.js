4da24dea1ccf67376966c1c14fbe13a70492ec02
Ritika Agrawal <ritikaagrawal339@gmail.com>
2023-05-11 16:31:09 +0530

    complete: Assignment II

---
+++ block-BNaabw/project/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+
+app.use(logger("dev"));
+
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  res.cookie("username", "ritika");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/users", (req, res) => {
+  res.send("Welcome to users!");
+});
+
+app.listen(4000, () => {
+  console.log("server listening on port 3000");
+});
